30d3fb20684d9d057fb73171b0a15c1cb1871964
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-11-23 13:38:07 +0700

    fix index.js for post

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const account = require("./api/account")
 const tag = require("./api/tag")
+const post = require("./api/post")
 
 const bodyParser = require("body-parser")
 
@@ -12,4 +13,5 @@ app.use(bodyParser.urlencoded({extended : false}))
 
 app.use("/account",account)
 app.use("/tag",tag)
+app.use("/post",post)
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
